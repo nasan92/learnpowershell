a875a04c1fee793185c1326009f04e2ea3b880ea
Santschi Nathanael <n.santschi@unico.ch>
2022-03-19 07:26:32 +0100

    update variables stuff

--- 02_Variables/00_Var.ps1
+++ 02_Variables/00_Var.ps1
@@ -20,5 +20,46 @@ Get-Variable
 
 
 # automatic variables
+# Find help to automatic variables: 
+help automatic_variables
+# Link: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_automatic_variables?view=powershell-7.2
 
+# $null Variable
+# contains null or empty value - explicit placeholder
 $null
+# If PowerShell is set to strict mode you'll get an error if you run $wine
+$wine
+# if you assign $null and run $wine then you won't get an error because PowerShell recoginzes the variable
+$wine = $null
+$wine
+$beer 
+
+# should show name wine with empty value
+get-variable -Name wine
+# shows error
+get-variable -Name beer
+
+# Note: $null is useful if you wan't to check if a variable is still empty or not.
+
+
+# LASTEXITCODE Variable
+# Contains the exit code of the last native program that was run.
+# PowerShell allows you to run external apps like ping.exe  - when 
+# 0 = success
+# 1 = failure
+
+$LASTEXITCODE
+PING.EXE -n 1 bieraufweindaslassesein.ch
+# Now lastexitcode should contain 1 because the website above doesn't exist
+$LASTEXITCODE
+
+# if we ping an existing domain and we have internet connection - lastexitcode will be 0
+PING.EXE -n 1 github.com
+$LASTEXITCODE
+
+
+
+# Verify which OS you are running:
+$IsLinux
+$IsMacOS
+$IsWindows
