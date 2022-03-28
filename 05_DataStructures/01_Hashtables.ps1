15fcb30a4bf0a47fbb0c4f8d61c9bfe065a5b2b1
Santschi Nathanael <n.santschi@unico.ch>
2022-03-28 15:52:11 +0200

    add hashtables + customobjects examples

--- 05_DataStructures/01_Hashtables.ps1
+++ 05_DataStructures/01_Hashtables.ps1
@@ -1,5 +1,6 @@
+# A hashtable is a data structure, much like an array, except you store each value (object) using a key. It's a basic key/value store.
 # Sometimes you want a way to correlate two pieces of data
-# example: you have a list of IP addresses that you wantch to match to computer names 
+# example: you have a list of computer names  that you wantch to match to  IP addresses
 # you give PowerShell an input - key (computername) and it returns value (ip adress for example)
 
 # Creating Hashtables
@@ -19,15 +20,16 @@ $ageList = @{
     Alex  = 9
 }
 
-# other example on multiple lines:
+# other example on multiple lines (lookup table):
 $environments = @{
     Prod = 'SrvProd05'
     QA   = 'SrvQA02'
     Dev  = 'SrvDev12'
 }
 
+$env = 'Prod'
 $server = $environments[$env]
-
+$server
 
 
 # Adding keys and values to Hashtables
@@ -56,6 +58,8 @@ $computerList.Values
 Select-Object -InputObject $computerList -Property *
 $computerList | select *
 
+# multiselection example
+$environments['QA','DEV']
 
 # Modifying Hashtables
 # ----------------------------------------------------------------------------------------------------
@@ -71,3 +75,73 @@ $computerList
 # ----------------------------------------------------------------------------------------------------
 $computerList.Remove('NB-NASA')
 $computerList
+
+
+
+# Splatting hashtables at cmdlets
+# ----------------------------------------------------------------------------------------------------
+# instead of including all parameters on one line:
+Copy-Item -Path "test.txt" -Destination "test2.txt" -WhatIf
+
+# you can use hash table splatting (put parameters into an hashtable)
+$HashArguments = @{
+    Path = "test.txt"
+    Destination = "test2.txt"
+    WhatIf = $true
+  }
+# Note the @ sign replaces the $ sign 
+Copy-Item @HashArguments
+
+
+# other splatting examples:
+$log = @{Path = '.\logfile.log'}
+Add-Content "logging this command" @log
+
+# multiple splats in one: 
+$Common = @{
+    SubnetMask  = '255.255.255.0'
+    LeaseDuration = (New-TimeSpan -Days 8)
+    Type = "Both"
+}
+
+$DHCPScope = @{
+    Name        = 'TestNetwork'
+    StartRange  = '10.0.0.2'
+    EndRange    = '10.0.0.254'
+    Description = 'Network for testlab A'
+}
+
+Add-DhcpServerv4Scope @DHCPScope @Common
+
+# Nested hashtables
+# ----------------------------------------------------------------------------------------------------
+# basic hashtable with two keys:
+$computer = @{
+    name    = 'NB-NASA'
+    ip      = '10.180.0.34'
+}
+$computer
+
+# creation of a new empty hashtable:
+$computer.os = @{}
+# adding new items to the os hashtable
+$computer.os.name       = 'Windows 11 Enterprise'
+$computer.os.version    = '10.0.22000 N/A Build 22000'
+
+
+# the same example inline:
+$computer2 = @{
+    name    = 'NB-NASA'
+    ip      = '10.180.0.34'
+    os      = @{
+        name    = 'Windows 11 Enterprise'
+        version = '10.0.22000 N/A Build 22000'
+    } 
+}
+$computer2
+# accessing properties:
+$computer2.os.version 
+$computer2.os['version']
+
+# to show all values you can use the converto-json
+$computer2 | ConvertTo-Json
---
+++ 05_DataStructures/02_CustomObjects.ps1
@@ -0,0 +1,60 @@
+# You can create custom objects with properties and methods that you define
+# you notice the difference to an hashtable when you want to use format-table or export-csv for example
+
+
+# Creating a PSCustomObject
+# ----------------------------------------------------------------------------------------------------
+$myBeer = [PSCustomObject]@{
+    Name            = 'Thun Bier'
+    Type            = 'Pale Ale'
+    Manufacturer    = 'Brauerei Thun AG'
+}
+$myBeer
+
+$myFirstCustomObject = [PSCustomObject]@{OSBuild = 'x'; OSVersion = 'y'}
+
+# legacy approach to create custom objects with the New-Object Command:
+$myFirstCustomObject = New-Object -TypeName PSCustomObject
+
+# Converting a hashtable to a PSCustomObject
+# ----------------------------------------------------------------------------------------------------
+# first we create a hashtable
+$myBeerHashtable = @{
+    Name            = 'Thun Bier'
+    Type            = 'Amber Ale'
+    Manufacturer    = 'Brauerei Thun AG'
+}
+$myBeerHashtable.GetType()
+$myBeerHashtable 
+
+# now we convert the hashtable to a pscustomobject
+$myBeer2 = [pscustomobject]$myBeerHashtable
+$myBeer2.gettype()
+$myBeer2
+
+
+# Adding properties
+# ----------------------------------------------------------------------------------------------------
+$myBeer2 | Add-Member -MemberType NoteProperty -Name 'Price' -Value 3.9
+# show the added property with the value:
+$myBeer2.Price
+
+
+# Reading / accessing properties
+# ----------------------------------------------------------------------------------------------------
+# show the added property with the value:
+$myBeer2.Price
+$myBeer2.Manufacturer
+# you can also use a string:
+$myBeer2.'Manufacturer'
+# or you can use a variable fo the property name:
+$property = 'Manufacturer'
+$myBeer2.$property
+
+Get-Member -InputObject $myBeer2 
+
+# Removing properties
+# ----------------------------------------------------------------------------------------------------
+$myBeer2.psobject.properties.remove('Price')
+$myBeer2
+
